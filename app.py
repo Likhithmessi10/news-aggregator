1296160a6cde6155a4f64ef3535750798bc8e15e
Likhith Anjaneya Gupta Mukkamala <mlikhith6@gmail.com>
2025-09-06 12:13:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -112,4 +112,5 @@ def refresh():
 
 if __name__ == "__main__":
     scrape_and_store()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",10000))
+    app.run(host="0.0.0.0",port=port,debug=False)
